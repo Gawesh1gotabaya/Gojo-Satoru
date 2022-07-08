fd7104dfd8328cb6eee908e821b10e089b694537
Gawesh1gotabaya <100393237+Gawesh1gotabaya@users.noreply.github.com>
2022-07-08 08:04:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+	'https://zenzapis.xyz': 'b49f1b5b5b84', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 
 //customize settings\\
